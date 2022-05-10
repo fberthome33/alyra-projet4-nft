2af85c561753a134d91402ba6c3f3044490a9edc
Fabien Berthomé <fabien.berthome.ext@veolia.com>
2022-05-10 12:07:15 +0200

    pinata sdk service

--- client/src/service/pushPinata.js
+++ client/src/service/pushPinata.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const key = process.env.PINATA_KEY;
 const secret = process.env.PINATA_SECRET;
+
+console.log('secret'  +secret);
 const pinataSDK = require('@pinata/sdk');
 const pinata = pinataSDK(key, secret);
 const fs = require('fs');
-const readableStreamForFile = fs.createReadStream('server.jpg');
+const readableStreamForFile = fs.createReadStream('./src/service/server.jpg');
 
 const options = {
     pinataMetadata: {
