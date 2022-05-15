bb616331078053342333a688474a07c193aefb4d
Fabien Berthomé <fabien.berthome.ext@veolia.com>
2022-05-16 00:58:21 +0200

    Updates

---
+++ precache-manifest.d88a78c9709f992cda2eb25d23950505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca59d3d5b75ba7291847093f7596a6f",
+    "url": "/alyra-projet4-nft/index.html"
+  },
+  {
+    "revision": "78227ff30895db6fb40e",
+    "url": "/alyra-projet4-nft/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "414d95b7c2dcc259ab92",
+    "url": "/alyra-projet4-nft/static/css/main.70e9a981.chunk.css"
+  },
+  {
+    "revision": "78227ff30895db6fb40e",
+    "url": "/alyra-projet4-nft/static/js/2.0f275538.chunk.js"
+  },
+  {
+    "revision": "414d95b7c2dcc259ab92",
+    "url": "/alyra-projet4-nft/static/js/main.1e9d7521.chunk.js"
+  },
+  {
+    "revision": "7b61cf83b0b77eece423",
+    "url": "/alyra-projet4-nft/static/js/runtime-main.b2693867.js"
+  }
+]);
