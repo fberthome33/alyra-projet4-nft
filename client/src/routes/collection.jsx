a84a11310f7f28488c21a6d5a8a0a7362156b90a
Pierre-Frédéric Murillo <pierrefrederic.murillo@gmail.com>
2022-05-15 13:57:22 +0200

    added collection images

--- client/src/routes/collection.jsx
+++ client/src/routes/collection.jsx
@@ -1,44 +1,56 @@
 import React, {useState, useEffect} from "react";
-import { useParams, useOutletContext } from "react-router-dom";
-import truncateEthAddress from 'truncate-eth-address';
+import { useNavigate, useParams, useOutletContext } from "react-router-dom";
 import Board from "../components/board";
 import Error from "./error";
 import Loading from "./loading";
 
 import useGetCollectionDetails from "../hooks/getcollectiondetails";
+import truncateEthAddress from "truncate-eth-address";
 
 const Collection = () => {
 
     const [user, web3, contracts] = useOutletContext();    
     const { address } = useParams();
-    const [loaded, setLoaded] = useState(false);
+    const navigate = useNavigate();
+    const getCollectionDetails = useGetCollectionDetails(web3, contracts.nftCollectionFactory);
     const [collection, setCollection] = useState();
-    //const [assets, setAssets] = useState([]);
+    const [loaded, setLoaded] = useState(false);
     
-    const collectionDetails = useGetCollectionDetails(web3);
-
     useEffect(async() => {
-        let collection = await collectionDetails(address);
-        //console.log(collection);
+        let collection = await getCollectionDetails(address);
         setCollection(collection);
         setLoaded(true);
     }, []);
 
-    if (!web3.utils.isAddress(address)) {
-        return <Error message="Requested collection not found..." />;
+    const createButton = () => {
+        if (web3.utils.toChecksumAddress(collection.creator) === web3.utils.toChecksumAddress(user.get('ethAddress')) ){
+            return (
+                <form className="createNew">
+                    <button onClick={() => { navigate('/collection/' + address + '/new'); }}>
+                        create new NFT
+                    </button>
+                </form>
+            )
+        }
     }
+
     if (!loaded) {
         return <Loading />;
+    } else if (!collection) {
+        return <Error message="Requested collection not found..." />;
     }
     return (
         <main>
             <h1>
                 Collection: {collection.name}<br/>
             </h1>
+            {createButton()}
+            <h2>details</h2>
             <p>
-                details:
-                {collection.creator}
+                created on: {collection.timestamp} <br />
+                created by: {truncateEthAddress(collection.creator)}
             </p>
+            <h2>NFT</h2>
             <Board type="asset" items={collection.assets} />
         </main>
     );
